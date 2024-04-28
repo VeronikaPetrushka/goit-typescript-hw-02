c3fb910c7477e2d637fa8c761135f3c823abbf65
Veronika <140621929+VeronikaPetrushka@users.noreply.github.com>
2024-04-28 22:09:02 +0200

    Delete my-react-app directory

--- my-react-app/src/components/ErrorMessage/ErrorMessage.tsx
+++
@@ -1,7 +0,0 @@
-const ErrorMessage: React.FC = () => {
-    return (
-        <p style={{fontSize: 20, color: "black"}}>Whooops...Something went wrong, please reload the page!</p>
-    );
-};
-
-export default ErrorMessage;
